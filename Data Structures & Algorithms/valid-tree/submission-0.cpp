0aedf3ddbf269b29c0839ad4419749d372307dd6
Ahmed Nageh <77284194+MangOo22@users.noreply.github.com>
2026-09-15 15:30:25 +0300

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.cpp
@@ -0,0 +1,60 @@
+class Solution {
+    
+    vector<vector<int>> graph;
+    vector<bool> visited;
+    bool isTree = true;
+
+    void constructGraph(int n,  vector<vector<int>>& edges,  vector<vector<int>>& graph) {
+        for (const auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+    }
+
+    bool hasCycle(int node, int parent) {
+        visited[node] = true;
+
+        for (int neighbour : graph[node]) {
+            if (neighbour == parent) {
+                continue;
+            }
+
+            if (visited[neighbour]) {
+                return true;
+            }
+
+            if (hasCycle(neighbour, node)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (n == 0) { return false; }
+
+        if (edges.size() != static_cast<size_t>(n - 1)) { return false; }
+
+        graph = vector<vector<int>>(n);
+        visited.assign(n, false);
+
+        constructGraph(edges.size(), edges, graph);
+
+         if (hasCycle(0, -1)) {
+            return false;
+        }
+
+        for (bool wasVisited : visited) {
+            if (!wasVisited) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+};
